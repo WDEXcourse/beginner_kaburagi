4eb325fa88de9f9e806db4d6c2181fad63a8266e
WDEXcourse <wdkawasaki.online@gmail.com>
2021-08-28 19:04:16 +0900

    Initial commit

---
+++ Assets/Scripts/WallDestroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PlayerMove playermove;//
+    void Start()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool FallFlag;//
+        FallFlag = playermove.fallFlag;//
+        if(FallFlag == true)
+        {
+            this.gameObject.SetActive(true);//
+        }
+    }
+
+    private void OnCollisionStay(Collision col)
+    {
+        //Destroy(this.gameObject);
+        this.gameObject.SetActive(false);
+    }
+}
